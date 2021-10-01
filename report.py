d16bee9c207c3872b801ac1fc22f7c1d7d818c69
sabeursamet <sametsabeur@gmail.com>
2021-10-01 14:52:20 +0100

    unittest

--- report.py
+++ report.py
@@ -10,10 +10,18 @@ class MyTestSuite(unittest.TestCase):
 
     def test_Issue(self):
         smoke_test = unittest.TestSuite()
-        smoke_test.addTests(
+        smoke_test.addTests([
             unittest.defaultTestLoader.loadTestsFromTestCase(wiki_unittest.MyWikiTest),
             unittest.defaultTestLoader.loadTestsFromTestCase(google_unittest.MyTestCase)
+        ])
+        outfile = open(direct+ "\SmokeTest.htm" , "w")
+
+        runner1 = HTMLTestRunner.HTMLTestRunner(
+            stress = outfile,
+            title = "Test Report",
+            description = "Smoke Tests"
         )
+        runner1.run(smoke_test)
 
 
 if __name__ == '__main__':
